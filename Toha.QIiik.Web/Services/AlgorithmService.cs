cb9d0487803a2027723be9a5f70cb2a52b3592fd
mtoha <mymail.mth@gmail.com>
2023-01-04 00:07:45 +0700

    remove dirt code

--- Toha.QIiik.Web/Services/AlgorithmService.cs
+++ Toha.QIiik.Web/Services/AlgorithmService.cs
@@ -15,52 +15,6 @@ namespace Toha.QIiik.Web.Services
         {
             AlgorithmResponse oResult = new AlgorithmResponse() { Algorithm = "MD5", Value = "" };
 
-            //using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
-            //{
-            //    byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(stringInput);
-            //    byte[] hashBytes = md5.ComputeHash(inputBytes);
-
-            //    StringBuilder sb = new System.Text.StringBuilder();
-            //    for (int i = 0; i < hashBytes.Length; i++)
-            //    {
-            //        sb.Append(hashBytes[i].ToString("X2"));
-            //    }
-            //    oResult.Value = sb.ToString();
-            //}
-
-            //MD5 md5 = new MD5CryptoServiceProvider();
-
-            ////compute hash from the bytes of text  
-            //md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(stringInput));
-
-
-            ////get hash result after compute it  
-            //byte[] result = md5.Hash;
-
-            //StringBuilder strBuilder = new StringBuilder();
-            //for (int i = 0; i < result.Length; i++)
-            //{
-            //    //change it into 2 hexadecimal digits  
-            //    //for each byte  
-            //    strBuilder.Append(result[i].ToString("x2"));
-            //}
-
-            //oResult.Value = strBuilder.ToString();
-
-
-            //using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
-            //{
-            //    byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(stringInput);
-            //    byte[] hashBytes = md5.ComputeHash(inputBytes);
-
-            //    StringBuilder sb = new StringBuilder();
-            //    for (int i = 0; i < hashBytes.Length; i++)
-            //    {
-            //        sb.Append(hashBytes[i].ToString("X2"));
-            //    }
-            //    oResult.Value = sb.ToString();
-            //}
-
 
             byte[] valueBytes = new byte[stringInput.Length]; // <-- don't multiply by 2!
 
